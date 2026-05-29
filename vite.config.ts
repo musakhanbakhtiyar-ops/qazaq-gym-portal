31578622eda97f30ea4c155449888087c6b0d4dc
musakhanbakhtiyar-ops <musakhanbakhtiyar@gmail.com>
2026-05-29 08:26:59 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +11,15 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
+  build: {
+    outDir: 'dist',
+  }
 });
